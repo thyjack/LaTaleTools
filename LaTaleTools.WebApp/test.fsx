e9655b9ef2015223c6acc5629776eec968d62740
Jack Liu <me@thejack.io>
2026-08-01 00:10:25 -0400

    Update package versions

--- LaTaleTools.WebApp/test.fsx
+++
@@ -1,8 +0,0 @@
-open System
-open System.Threading.Tasks
-
-type Test() =
-    interface IAsyncDisposable with
-        member _.DisposeAsync() =
-            printfn "Disposing"
-            ValueTask()
